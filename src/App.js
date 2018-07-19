c367e238e18189156aca107e66c1feea84c4dcaa
MelroySanthmayor <melsanthmayor87@gmail.com>
2018-07-19 14:54:53 +0530

    Added Additem component and functions to handle add and delete

--- src/App.js
+++ src/App.js
@@ -1,19 +1,56 @@
 import React, { Component } from 'react';
 import User from './components/User'
+import image from './dummy-user.png'
 import './App.css';
+import Itemlist from './components/Itemlist'
+import AddItem from './components/AddItem'
 
 class App extends Component {
-  
+  state = {
+    items:[{image:image,name:'Ring',type:'1',material:'gold'},
+           {image:image,name:'Necklace',type:'2',material:'silver'},
+           {image:image,name:'Bracelet',type:'3',material:'platinum'}
+
+    ]
+  }
   render() {
+    var items = this.state.items;
+    items = items.map(
+      function(item, index) 
+      {
+        return <Itemlist item={item} key={index} onDelete={this.onDelete} />;
+      }.bind(this)
+    );
     return (
       <div className="App">
       <nav>
         <header id="header">Jewellery Collection</header>
       </nav>
         <User/>
+        <ul>{items}</ul>
+        <AddItem onAdd={this.onAdd}/>
+
       </div>
     );
   }
+  //Custom Functions
+  onDelete(item,key) {
+    var i = this.state.items.indexOf(item);
+    
+    this.state.items.splice(i,1);
+
+  }
+  onAdd(item){
+      var updateditems = this.state.items
+      updateditems.push(item);
+      this.setState({
+          //items: updatedtodos
+          items : [...updateditems,
+          {image:image,name:'Ring',type:'1',material:'gold'}]// trying to render one static addition of the input
+      })
+
+  }
+
 }
 
 export default App;
