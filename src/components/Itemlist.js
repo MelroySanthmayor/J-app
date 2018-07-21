4e965f8f6a21221439b9279afdc80c1c85bd6e27
MelroySanthmayor <melsanthmayor87@gmail.com>
2018-07-21 15:04:24 +0530

    Component to render Items and delete on click event handled

--- src/components/Itemlist.js
+++ src/components/Itemlist.js
@@ -9,19 +9,34 @@ export default class Itemlist extends Component{
 
         return(
            
-        <li>
-          <div className="todo-item">
-            <span className="item-name">{this.props.item}</span>
-            <span className="item-delete" onClick={this.handleDelete}> x </span>
-          </div>
-        </li>
-           
+            <li  className="grid-item">
+                    
+            <img className="Img" src={this.props.image} alt="error" />
+            <div id="myModal" class="modal">
+            <div class="modal-content">
+            <div class="modal-header">    
+            <span class="close">&times;</span>
+               <h2>Item Details</h2>
+            </div>
+            <div class="modal-body">
+              <p>Type:{this.props.type}</p>
+              <p>Material:{this.props.material}</p>
+            </div>
+            <div class="modal-footer">
+            <h3>{this.props.name}</h3>
+            </div>
+            </div>
+            </div>
+            <div className="container1">
+            {this.props.name}
+            </div>
+            <span class="close1" onClick={this.props.Delete}>&times;</span>
+            
+          </li>
 
 
         );
 
     }
-    handleDelete() {
-        this.props.onDelete(this.props.item);
-    }
+  
 }
